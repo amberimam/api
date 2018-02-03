4e62cf81166458147ec61b8913014f72f1ee7697
Amber Imam <amber.imam@gmail.com>
2018-02-02 17:21:11 -0800

    Implement error checking for product ids that don't exit, return appropriate HTTP status codes, add id to product object when reading in from db

--- api.py
+++ api.py
@@ -4,18 +4,30 @@ from flask import Flask
 from flask_restful import Resource, Api
 import json
 
+
+def init_product_list():
+    db = open('db.json', 'r')
+    products = json.loads(db.read())
+    db.close()
+
+    for index, product in enumerate(products):
+        product['id'] = index
+
+    return products
+
+
 app = Flask(__name__)
 api = Api(app)
-
-db = open('db.json', 'r')
-products = json.loads(db.read())
-db.close()
+products = init_product_list()
 
 #import pdb; pdb.set_trace()
 
 class Product(Resource):
     def get(self, product_id):
-        return products[product_id]
+        if product_id >= 0 and product_id < len(products):
+            return products[product_id]
+        else:
+            return None, 404
 
 class Products(Resource):
     def get(self):
@@ -23,6 +35,8 @@ class Products(Resource):
 
 api.add_resource(Product, '/products/<int:product_id>')
 api.add_resource(Products, '/products')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
