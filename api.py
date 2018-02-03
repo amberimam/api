bccf98344046e574e6124f2caf961ca83dc2d839
Amber Imam <amber.imam@gmail.com>
2018-02-02 21:39:39 -0800

    Update db schema to use ints and floats for numerical values instead of strings, add support for simple filtering by category

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import abort, Resource, Api
 import json
+from webargs import fields
+from webargs.flaskparser import use_kwargs
 
 
 def init_product_list():
@@ -20,22 +22,27 @@ app = Flask(__name__)
 api = Api(app)
 products = init_product_list()
 
-#import pdb; pdb.set_trace()
-
 class Product(Resource):
     def get(self, product_id):
         if product_id >= 0 and product_id < len(products):
             return products[product_id]
         else:
-            return None, 404
+            abort(404, message="Product {} doesn't exist".format(product_id))
 
 class Products(Resource):
-    def get(self):
-        return products
+    args = {
+        'category': fields.Str(required=False)
+    }
+
+    @use_kwargs(args)
+    def get(self, category):
+        if category:
+            return [p for p in products if category in p['categories']]
+        else:
+            return products
 
 api.add_resource(Product, '/products/<int:product_id>')
 api.add_resource(Products, '/products')
-
 
 
 if __name__ == '__main__':
